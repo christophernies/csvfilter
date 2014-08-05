21efccdf1733e014b58cb8691211511f0cc0cb39
Christopher Nies <christophernies@gmail.com>
2014-08-05 19:57:44 -0400

    added schema creation function to determine order of fields

--- csv-reader.py
+++ csv-reader.py
@@ -1,6 +1,23 @@
 import sys
 import csv
 
-data_file = 'data.csv'
+source_file = 'data.csv'
 filter_set = {}
+schema = []
 
+def open_file(source_file):
+    csvfile = open(source_file,'r')
+    csvreader = csv.reader(csvfile, delimiter=',')
+    return csvreader
+
+def apply_filter(filters,row):
+    print 'lol'
+
+def create_schema(data):
+    return data.next()
+
+data = open_file(source_file)
+schema = create_schema(data)
+
+for row in data:
+    apply_filter(filter_set,row)
